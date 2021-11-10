727006348e4899661048402d18dc792c28272f58
MANASVI YADAV <manasviyadav330@gmail.com>
2021-11-10 23:21:03 +0530

    Add files via upload

---
+++ BMI Calculator/calculator.js
@@ -0,0 +1,39 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser= require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.get("/", function(req,res)
+{
+
+  res.sendFile(__dirname + "/index.html");
+
+});
+app.get("/bmiCalculator", function(req,res)
+{
+
+  res.sendFile(__dirname + "/bmiCalculator.html");
+
+});
+app.post("/",function(req,res)
+{
+  var num1=Number(req.body.num1);
+  var num2=Number(req.body.num2);
+  var result=num1+num2;
+
+    res.send("The result is: " + result);
+});
+app.post("/bmiCalculator",function(req,res)
+{
+  var w=parserFloat(req.body.weight);
+  var h=parserFloat(req.body.height);--
+  var bmi=w/(h*h);
+
+    res.send("The result is: " + bmi);
+});
+app.listen(3000,function()
+{
+  console.log("Server started at port 3000.");
+});
